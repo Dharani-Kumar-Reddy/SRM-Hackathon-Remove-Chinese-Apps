0569d4ad1b316264d1561e4804c5fee4523c179b
Dharani-Kumar-Reddy <dharanikumarreddy23@gmail.com>
2020-07-22 00:36:26 +0530

    Read data from firebase and find prohibited apps

---
+++ app/src/main/java/com/example/removechineseapps/App.java
@@ -0,0 +1,40 @@
+package com.example.removechineseapps;
+
+public class App {
+    private String app_logo;
+    private String app_name;
+    private String app_package;
+
+    public App(String app_logo, String app_name, String app_package) {
+        this.app_logo = app_logo;
+        this.app_name = app_name;
+        this.app_package = app_package;
+    }
+
+    public App() {
+    }
+
+    public String getApp_logo() {
+        return app_logo;
+    }
+
+    public void setApp_logo(String app_logo) {
+        this.app_logo = app_logo;
+    }
+
+    public void setApp_name(String app_name) {
+        this.app_name = app_name;
+    }
+
+    public void setApp_package(String app_package) {
+        this.app_package = app_package;
+    }
+
+    public String getApp_name() {
+        return app_name;
+    }
+
+    public String getApp_package() {
+        return app_package;
+    }
+}
--- app/src/main/java/com/example/removechineseapps/MainActivity.java
+++ app/src/main/java/com/example/removechineseapps/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.removechineseapps;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,27 +27,71 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+    public void scan(View v){
+        //get data and check for each app in device
+        List<App> appList=getPackages();
+        //pass the above list to recycler view below
+
+
+    }
+    private List<App> getPackages(){
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        DatabaseReference myRef = database.getReference("Apps");
 
-        myRef.setValue("Hello, Dheeru");
-
-        myRef.addValueEventListener(new ValueEventListener() {
+        List<App> apps_to_be_removed=new ArrayList<>();
+        final List<App> apps_list=new ArrayList<>();
+        myRef.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + value);
-                Toast.makeText(MainActivity.this, value, Toast.LENGTH_SHORT).show();
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                App app_obj = snapshot.getValue(App.class);
+                apps_list.add(app_obj);
+                Log.i("Name",Integer.toString(apps_list.size()));
+
             }
 
             @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
+        for(int pos=0;pos<apps_list.size();pos++){
+            App obj=apps_list.get(pos);
+            if(isAppPresent(obj.getApp_package())){
+                apps_to_be_removed.add(obj);
+            }
+        }
+        return apps_to_be_removed;
+    }
 
+    private Boolean isAppPresent(String uri){
+            PackageManager pm = getPackageManager();
+            try {
+                pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+                return true;
+            } catch (PackageManager.NameNotFoundException e) {
+            }
+
+            return false;
     }
 }
